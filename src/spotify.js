d4a0770c56dbbb42ddca459072d4784c654d9ed2
José Peraza <joseperazarivero@gmail.com>
2021-05-11 16:14:39 -0500

    arreglando el login

--- src/spotify.js
+++ src/spotify.js
@@ -1,5 +1,5 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-react-app-challenge.netlify.app/";
 const clientId = "d3a1dbdcd3664b8fb4101206df01c0e0";
 
 
